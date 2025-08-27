664b236c4e47543c1d307fb2317eb3db7205b19b
Herry Patel <85663924+herry73@users.noreply.github.com>
2025-08-27 10:08:31 +0200

    created server function and added message types

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 const Port = "6969"
-const safeMode = true
+const safeMode = false
 
 func safeRemoteAddr(conn net.Conn) string {
 	if safeMode {
@@ -16,18 +16,68 @@ func safeRemoteAddr(conn net.Conn) string {
 	}
 }
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	message := []byte("Hallo meine Freunde! Kraut!\n")
-	n, err := conn.Write(message)
-	if err != nil {
-		log.Printf("Could not write message to %s\n", safeRemoteAddr(conn))
-		return
-	}
-	if n < len(message) {
-		log.Printf("The message was not fully written %d/%d \n", n, len(message))
-	}
+type MessageType int
 
+const (
+	ClientConnected MessageType = iota + 1
+	DeleteClient
+	NewMessage
+)
+
+type Message struct {
+	Type MessageType
+	Conn net.Conn
+	Text string
+}
+
+type Client struct {
+	conn     net.Conn
+	outgoing chan string
+}
+
+func server(messages chan Message) {
+	conns := map[string]net.Conn{}
+	for {
+		msg := <-messages
+		switch msg.Type {
+		case ClientConnected:
+			conns[msg.Conn.RemoteAddr().String()] = msg.Conn
+		case DeleteClient:
+			delete(conns, msg.Conn.RemoteAddr().String())
+		case NewMessage:
+			senderaddr := msg.Conn.RemoteAddr().String()
+			for addr, conn := range conns {
+				if addr != senderaddr {
+					_, err := conn.Write([]byte(msg.Text))
+					if err != nil {
+						//left: remove connection from list
+						log.Printf("Could not send data to %s: %s", safeRemoteAddr(conn), err)
+					}
+				}
+			}
+		}
+	}
+}
+
+func client(conn net.Conn, messages chan Message) {
+	buffer := make([]byte, 512)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			conn.Close()
+			messages <- Message{
+				Type: DeleteClient,
+				Conn: conn,
+			}
+			return
+		}
+		messages <- Message{
+			Type: NewMessage,
+			Text: string(buffer[0:n]),
+			Conn: conn,
+		}
+
+	}
 }
 
 func main() {
@@ -35,17 +85,24 @@ func main() {
 	ln, err := net.Listen("tcp", ":"+Port)
 	if err != nil {
 		// handle error
-		log.Fatalf("ERROR: Could not listen to epic port %s: %s\n", Port, err)
+		log.Fatalf("ERROR: Could not listen to port %s: %s\n", Port, err)
 
 	}
 	log.Printf("Listening to TCP Connections on Port %s ....\n", Port)
+
+	messages := make(chan Message)
+	go server(messages)
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			//handle error
-			log.Println("ERROR: Could not accept a connection: %s\n", err)
+			log.Printf("ERROR: Could not accept a connection: %s\n", err)
 		}
 		log.Printf("Connection accepted from %s\n", safeRemoteAddr(conn))
-		go handleConnection(conn)
+		messages <- Message{
+			Type: ClientConnected,
+			Conn: conn,
+		}
+		go client(conn, messages)
 	}
 }
